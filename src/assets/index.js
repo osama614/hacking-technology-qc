f7a173d340a4a9d6313e82deaad509bac81912d9
monaa_mohamed <mona.mohamed@intellisc.com>
2021-02-25 23:03:37 +0200

    add used assets

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import whiteLogo from './images/white-logo.svg';
+import leftArrow from './images/left-arrow.svg';
+import telegramIcon from './images/telegram-icon.svg';
+import twitterIcon from './images/twitter-icon.svg';
+import facebookIcon from './images/facebook-icon.svg';
+
+export const WhiteLogo = whiteLogo;
+export const LeftArrow = leftArrow;
+export const TelegramIcon = telegramIcon;
+export const TwitterIcon = twitterIcon;
+export const FacebookIcon = facebookIcon;
