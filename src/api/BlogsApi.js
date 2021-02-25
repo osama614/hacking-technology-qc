06e9b5997e8002dde6a5df35abeb8684c4a18d41
monaa_mohamed <mona.mohamed@intellisc.com>
2021-02-25 23:03:54 +0200

    add api services

---
+++ src/api/BlogsApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { apiUrl } from "./Constants";
+
+const blogsURL = apiUrl + "/blogs";
+
+export const getCategoriesList = () => axios.get(`${blogsURL}/categories/`);
+
+export const getBlogsList = () => axios.get(`${blogsURL}/`);
+
+export const getBlogDetails = (id) => axios.get(`${blogsURL}/${id}/`);
+
+export const searchByCategory = (category) =>
+  axios.get(`${blogsURL}/search/categories?category=${category}/`);
+
+export const searchByTag = (tag) =>
+  axios.get(`${blogsURL}/search/tags?tag=${tag}/`);
---
+++ src/api/Constants.js
@@ -0,0 +1,3 @@
+//base url
+export const baseUrl = "http://osos.pythonanywhere.com";
+export const apiUrl = "http://osos.pythonanywhere.com/api/v1";
