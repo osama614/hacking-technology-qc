728258c9282ad8f4401e8cd030ffcffac47055e2
monaa_mohamed <mona.mohamed@intellisc.com>
2021-02-28 11:27:32 +0200

    edit app routes

--- src/modules/Blog/components/Home/Home.js
+++ src/modules/Blog/components/Home/Home.js
@@ -1,16 +1,22 @@
 import React, { useEffect, Fragment } from "react";
 import { connect } from "react-redux";
-import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
+import { Switch, Route, withRouter, BrowserRouter } from "react-router-dom";
 
-import BlogsList from "../BlogsList";
 import Navbar from "../layout/Navbar";
 import Footer from "../layout/Footer";
+import AboutUs from "../AboutUs";
+import ContactUs from "../ContactUs";
 import FeaturedBlogs from "./FeaturedBlogs";
+import BlogsList from "../BlogsList";
+import PrivacyPolicy from "../PrivacyPolicy";
+import TermsOfUse from "../TermsOfUse";
 
 import { handleGetCategories, handleGetBlogs } from "../../actions/index";
 import Blog from "../Blog";
 
-const Home = ({ dispatch }) => {
+const Home = (props) => {
+  const { dispatch } = props;
+
   useEffect(() => {
     dispatch(handleGetCategories());
     dispatch(handleGetBlogs());
@@ -24,25 +30,31 @@ const Home = ({ dispatch }) => {
   );
 
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
+    <BrowserRouter>
+      <div>
+        <Navbar currentPathname={props.location.pathname} />
         <div className="container home">
-          <>
-            <Route path="/" exact component={homeContainer} />
-            <Route path="/:id" component={Blog} />
-            <Route
-              path={["/http:", "/https:"]}
-              component={(props) => {
-                window.location.replace(props.location.pathname.substr(1));
-                return null;
-              }}
-            />
-          </>
+          <Switch>
+            <Fragment>
+              <Route
+                path={["/http:", "/https:"]}
+                component={(props) => {
+                  window.location.replace(props.location.pathname.substr(1));
+                  return null;
+                }}
+              />
+              <Route path="/about-us" component={AboutUs} />
+              <Route path="/contact-us" component={ContactUs} />
+              <Route path="/privacy-policy" component={PrivacyPolicy} />
+              <Route path="/terms-of-use" component={TermsOfUse} />
+              <Route path="/:id" component={Blog} />
+              <Route exact path="/" component={homeContainer} />
+            </Fragment>
+          </Switch>
         </div>
         <Footer />
-      </Fragment>
-    </Router>
+      </div>
+    </BrowserRouter>
   );
 };
 
