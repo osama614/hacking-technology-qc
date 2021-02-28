5965000d3e922c17fdb84df658a0538cba73fe55
monaa_mohamed <mona.mohamed@intellisc.com>
2021-02-28 11:27:07 +0200

    edit navbar ui

--- src/modules/Blog/components/layout/Navbar.js
+++ src/modules/Blog/components/layout/Navbar.js
@@ -1,26 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import { WhiteLogo } from "../../../../assets/index";
 
-const Navbar = () => {
+const Navbar = ({ currentPathname }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeTab, setActiveTab] = useState("home");
+
+  useEffect(() => {
+    switch (currentPathname) {
+      case "/about-us":
+        setActiveTab("about-us");
+        break;
+      case "/contact-us":
+        setActiveTab("contact-us");
+        break;
+      default:
+        setActiveTab("home");
+        break;
+    }
+  }, [currentPathname]);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark blog-nav">
       <Link className="navbar-brand" to="/">
         <img src={WhiteLogo} width={30} height={30} alt="logo" loading="lazy" />
       </Link>
-      <ul className="navbar-nav">
-        <li className="nav-item active">
-          <Link className="nav-link" to="/">
-            المدونة <span className="sr-only">(current)</span>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/about">
-            من نحن
-          </Link>
-        </li>
-      </ul>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        onClick={toggle}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="collapse navbar-collapse"
+        id="navbarNav"
+        style={{ display: isOpen ? "block" : "none" }}
+      >
+        <ul className="navbar-nav">
+          <li
+            className={`nav-item ${activeTab === "home" ? "active" : ""}`}
+            id="home"
+          >
+            <Link className="nav-link" to="/">
+              المدونة <span className="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li
+            className={`nav-item ${activeTab === "about-us" ? "active" : ""}`}
+            id="about-us"
+          >
+            <Link className="nav-link" to="/about-us">
+              من نحن
+            </Link>
+          </li>
+          <li
+            className={`nav-item ${activeTab === "contact-us" ? "active" : ""}`}
+            id="contact-us"
+          >
+            <Link className="nav-link" to="/contact-us">
+              اتصل بنا
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
