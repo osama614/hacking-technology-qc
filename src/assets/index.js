782a312b71190a47e6cee07b1c979e3137d3dd23
monaa_mohamed <mona.mohamed@intellisc.com>
2021-02-28 11:26:10 +0200

    add footer

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,9 +3,17 @@ import leftArrow from './images/left-arrow.svg';
 import telegramIcon from './images/telegram-icon.svg';
 import twitterIcon from './images/twitter-icon.svg';
 import facebookIcon from './images/facebook-icon.svg';
+import twitterLogo from './images/twitter-logo.svg';
+import facebookLogo from './images/facebook-logo.svg';
+import linkedInLogo from './images/linkedin-logo.svg';
+import youTubeLogo from './images/youtube-logo.svg';
 
 export const WhiteLogo = whiteLogo;
 export const LeftArrow = leftArrow;
 export const TelegramIcon = telegramIcon;
 export const TwitterIcon = twitterIcon;
 export const FacebookIcon = facebookIcon;
+export const TwitterLogo = twitterLogo;
+export const FacebookLogo = facebookLogo;
+export const LinkedInLogo = linkedInLogo;
+export const YouTubeLogo = youTubeLogo;
--- src/modules/Blog/components/layout/Footer.js
+++ src/modules/Blog/components/layout/Footer.js
@@ -1,15 +1,92 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import {
+  FacebookLogo,
+  TwitterLogo,
+  YouTubeLogo,
+  LinkedInLogo,
+} from "../../../../assets/index";
 
 const Footer = () => {
   return (
     <>
+      <div className="footer mt-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="footer-col first">
+                <h5 style={{ color: "rgb(243, 247, 253)" }}>
+                  Hacking Technology
+                </h5>
+                <p className="p-small py-3">
+                  شركة{" "}
+                  <strong style={{ color: "#08cc96" }}>
+                    Hacking Technology
+                  </strong>{" "}
+                  هي شركة عربية برؤية عالمية، تسعى لخلق فضاء الكتروني آمن، وجعل
+                  الانترنت مكاناً أفضل.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="footer-col first">
+                <h5 style={{ color: "rgb(243, 247, 253)" }}>الاتصال بنا</h5>
+                <p className="p-small py-3">
+                  يمكنكم الاتصال بنا عند الحاجة من خلال الضغط على رابط اتصل بنا
+                  المتوفر في روابط موقعنا أو الإرسال إلى بريدنا الالكتروني على
+                  اسم النطاق أعلاه
+                </p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="footer-col last">
+                <h5 style={{ color: "rgb(243, 247, 253)" }}>
+                  وسائل التواصل الاجتماعي
+                </h5>
+                <ul className="media-list li-space-lg p-small d-flex px-0 py-3">
+                  <li className="media">
+                    <Link to="/">
+                      <img src={FacebookLogo} alt="facebook logo" />
+                    </Link>
+                  </li>
+                  <li className="media">
+                    <Link to="/">
+                      <img src={TwitterLogo} alt="twitter logo" />
+                    </Link>
+                  </li>
+
+                  <li className="media">
+                    <Link to="/">
+                      <img src={YouTubeLogo} alt="youtube logo" />
+                    </Link>
+                  </li>
+                  <li className="media">
+                    <Link to="/">
+                      <img src={LinkedInLogo} alt="linkedin logo" />
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="copyright">
         <div className="container">
           <div className="row">
-            <div className="col-lg-12">
-              <p className="p-small text-center text-muted">
-                © جميع الحقوق محفوظة <span id="ftDate"> 2021</span>
+            <div className="col-lg-12 d-flex justify-content-between">
+              <p className="p-small text-muted">
+                جميع الحقوق محفوظة ©<span id="ftDate"> 2021</span>
               </p>
+              <div className="d-flex justify-content-between privacy-terms">
+                <Link className="p-small mx-2" to="/privacy-policy">
+                  سياسة الخصوصية
+                </Link>
+                <Link className="p-small mx-2" to="/terms-of-use">
+                  اتفاقية الاستخدام
+                </Link>
+              </div>
             </div>
           </div>
         </div>
