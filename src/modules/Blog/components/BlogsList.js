046d5f33b48ecbb7bdc89659332705672451eaa6
monaa_mohamed <mona.mohamed@intellisc.com>
2021-03-01 18:52:19 +0200

    edit filters and sort to be dropdown

--- src/modules/Blog/components/BlogsList.js
+++ src/modules/Blog/components/BlogsList.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 
+import CustomSelect from "../../../shared/components/FormFields/CustomSelect";
 import BlogCard from "./BlogCard";
 import Pagination from "./layout/Pagination";
 
 const BlogsList = ({ categories, blogsList }) => {
-
   const perPage = 5;
+  const sortOptions = [
+    { id: 0, label: "الأحدث", value: "الأحدث" },
+    { id: 1, label: "الأقدم", value: "الأقدم" },
+  ];
+  const [categoriesFilters, setCategoriesFilters] = useState([]);
   const [pageCount, setPageCount] = useState(1);
   const [sortedBlogs, setSortedBlogs] = useState([]);
   const [currentPageBlogs, setCurrentPageBlogs] = useState([]);
-  const [currentFilter, setCurrentFilter] = useState(0);
-  const [currentSort, setCurrentSort] = useState(0);
+  const [currentFilter, setCurrentFilter] = useState(null);
+  const [currentSort, setCurrentSort] = useState({ id: 0, label: "الأحدث", value: "الأحدث" });
 
   useEffect(() => {
     let sortedBlogs = [...blogsList];
@@ -19,31 +24,43 @@ const BlogsList = ({ categories, blogsList }) => {
 
     if (currentFilter) {
       sortedBlogs = blogsList.filter(
-        (blog) => blog.category === currentFilter
+        (blog) => blog.category === currentFilter.id
       );
       blogsCount = sortedBlogs.length;
     }
 
-    if (currentSort === 0) {
+    if (currentSort.id === 0) {
       sortedBlogs.sort((a, b) => new Date(b.publish) - new Date(a.publish));
-    }
-    else{
+    } else {
       sortedBlogs.sort((a, b) => new Date(a.publish) - new Date(b.publish));
     }
-    
+
     setSortedBlogs([...sortedBlogs]);
     setCurrentPageBlogs([...sortedBlogs.slice(0, perPage)]);
     setPageCount(blogsCount / perPage);
   }, [blogsList, currentFilter, currentSort]);
 
-  const onFilterChange = ({ target }) => {
-    const filter = Number(target.value);
+  useEffect(() => {
+    const filters = [];
+
+    if (categories) {
+      categories.forEach((category) =>
+        filters.push({
+          id: category.id,
+          label: category.title,
+          value: category.title,
+        })
+      );
+      setCategoriesFilters([...filters]);
+    }
+  }, [categories]);
+
+  const onFilterChange = (filter) => {
     setCurrentFilter(filter);
   };
 
-  const onSortChange = ({ target }) => {
-    const sortOption = Number(target.value);
-    setCurrentSort(sortOption);
+  const onSortChange = (option) => {
+    setCurrentSort(option);
   };
 
   const handlePageClick = (data) => {
@@ -55,102 +72,43 @@ const BlogsList = ({ categories, blogsList }) => {
   return (
     <div className="blogs-list mb-4">
       <div className="blogs-list-title mb-4 text-center">
-        <h2>{
-        currentSort === 0? 
-        "أحدث المدونات"
-        :"أقدم المدونات"}</h2>
+        <h2>{currentSort === 0 ? "أحدث المدونات" : "أقدم المدونات"}</h2>
       </div>
       <div className="blogs-list-body row">
-        <div className="sidenav col-md-2">
-          <p className="filters-title">فلترة بحسب الفئة</p>
-          <div className="form-group row categories-filters">
-            <div className="col-sm-10">
-              <div className="form-check">
-                <input
-                  className={`form-check-input ${
-                    currentFilter === 0 ? "checked" : ""
-                  }`}
-                  type="radio"
-                  name="categoryFilter"
-                  value={0}
-                  id="0"
-                  checked={currentFilter === 0}
-                  onChange={onFilterChange}
-                />
-                <label className="form-check-label" htmlFor={0}>
-                  الكل
-                </label>
-              </div>
-              {categories.map((category) => (
-                <div className="form-check" key={category.id}>
-                  <input
-                    className={`form-check-input ${
-                      currentFilter === category.id ? "checked" : ""
-                    }`}
-                    type="radio"
-                    id={category.id}
-                    name="categoryFilter"
-                    value={category.id}
-                    checked={currentFilter === category.id}
-                    onChange={onFilterChange}
-                  />
-
-                  <label className="form-check-label" htmlFor={category.id}>
-                    {category.title}
-                  </label>
-                </div>
-              ))}
-            </div>
-          </div>
-        
-          <p className="sort-title">ترتيب حسب</p>
-          <div className="form-group row sort-options">
-            <div className="col-sm-10">
-              <div className="form-check">
-                <input
-                  className={`form-check-input ${
-                    currentSort === 0 ? "checked" : ""
-                  }`}
-                  type="radio"
-                  name="sortOption"
-                  value={0}
-                  id="sort-new"
-                  checked={currentSort === 0}
-                  onChange={onSortChange}
-                />
-                <label className="form-check-label" htmlFor="sort-new">
-                  الأحدث
-                </label>
-              </div>
-              <div className="form-check">
-                <input
-                  className={`form-check-input ${
-                    currentSort === 1 ? "checked" : ""
-                  }`}
-                  type="radio"
-                  name="sortOption"
-                  value={1}
-                  id="sort-old"
-                  checked={currentSort === 1}
-                  onChange={onSortChange}
-                />
-                <label className="form-check-label" htmlFor="sort-old">
-                  الأقدم
-                </label>
-              </div>
-            </div>
+        <div className="sidenav col-md-3">
+          <CustomSelect
+            id="category"
+            value={currentFilter}
+            options={categoriesFilters}
+            isClearable={true}
+            isSearchable={true}
+            placeholder="الفئة"
+            label="فلترة بحسب الفئة"
+            classNames="form-group w-100 categories-filters"
+            onChange={onFilterChange}
+          />
+          <CustomSelect
+            id="sort"
+            value={currentSort}
+            options={sortOptions}
+            isSearchable={true}
+            label="ترتيب حسب"
+            classNames="form-group w-100 sort-options"
+            onChange={onSortChange}
+          />
         </div>
-        </div>
-        <div className="main p-0 col-md-10">
+        <div className="main p-0 col-md-9">
           {sortedBlogs.length > 0 ? (
             currentPageBlogs.length > 0 &&
             currentPageBlogs.map((blog) => (
               <BlogCard
                 key={blog.id}
                 blog={blog}
-                category={categories.filter(
-                  (category) => category.id === blog.category
-                )[0]}
+                category={
+                  categories.filter(
+                    (category) => category.id === blog.category
+                  )[0]
+                }
               />
             ))
           ) : (
