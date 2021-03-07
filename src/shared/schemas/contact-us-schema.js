2ce472961efaac29fe93a680f372f2a040a718b0
monaa_mohamed <mona.mohamed@intellisc.com>
2021-03-07 22:25:44 +0200

    add name to contact us schema

--- src/shared/schemas/contact-us-schema.js
+++ src/shared/schemas/contact-us-schema.js
@@ -1,26 +1,37 @@
-import Joi from 'joi';
+import Joi from "joi";
 
-export const emailErrorMsg = 'يجب إن يكون بريد إلكتروني صالح!';
-export const requiredErrorMsg = '!هذا الحقل مطلوب';
+export const emailErrorMsg = "يجب إن يكون بريد إلكتروني صالح!";
+export const requiredErrorMsg = "!هذا الحقل مطلوب";
 
 export const contactUsSchema = Joi.object({
-    email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'edu'] } }).message(emailErrorMsg)
-        .required().messages({
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "edu"] } })
+    .message(emailErrorMsg)
+    .required()
+    .messages({
       "string.empty": requiredErrorMsg,
-      "any.required": requiredErrorMsg
+      "any.required": requiredErrorMsg,
     })
-        .label("Email"),
-    subject: Joi.string()
-        .required().messages({
+    .label("Email"),
+  name: Joi.string()
+    .required()
+    .messages({
       "string.empty": requiredErrorMsg,
-      "any.required": requiredErrorMsg
+      "any.required": requiredErrorMsg,
     })
-        .label("Subject"),
-    message: Joi.string()
-        .required().messages({
+    .label("Name"),
+  subject: Joi.string()
+    .required()
+    .messages({
       "string.empty": requiredErrorMsg,
-      "any.required": requiredErrorMsg
+      "any.required": requiredErrorMsg,
     })
-        .label("Message")
+    .label("Subject"),
+  message: Joi.string()
+    .required()
+    .messages({
+      "string.empty": requiredErrorMsg,
+      "any.required": requiredErrorMsg,
+    })
+    .label("Message"),
 });
